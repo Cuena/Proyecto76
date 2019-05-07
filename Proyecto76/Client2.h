aa1c5bd1138f4aa4ff7a1656356134273d545151
Ima <imanolgonzalezestepa@opendeusto.es>
2019-05-07 16:12:30 +0200

    El cliente para el online (Cliente2())

---
+++ Proyecto76/Client2.h
@@ -0,0 +1,35 @@
+#pragma once
+#include <WS2tcpip.h>
+#include <thread>
+extern "C" {
+#include "Juego76.h"
+}
+#include <string>
+#include <iostream>
+#include <sstream>
+#pragma comment (lib, "ws2_32.lib")
+
+class Client2
+{
+private:
+	std::string ipAddress = "25.68.58.155";
+	int port = 2620;
+	WSAData data;
+	WORD ver;
+	SOCKET sock;
+	sockaddr_in hint;
+	char buf[4096];
+	std::string userInput;
+	char* jugador;
+	bool toca2=false;
+	bool* toca=&toca2;
+
+public:
+	Client2();
+	void winSockInit();
+	void connectServer();
+	
+
+	~Client2();
+};
+
--- Proyecto76/main.cpp
+++ Proyecto76/main.cpp
@@ -1,7 +1,9 @@
 #include "IaGUI.h"
-
+#include "Client2.h"
+#include "Sfmlcon.h"
 int main() {
 
 	IaGUI(true); //true -> IA  |  false -> PvP
+	//Client2();
 	return 0;
 }
