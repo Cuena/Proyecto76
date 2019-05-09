25f8257abde2d60d42469e2bde7559ef374b9571
Ima <imanolgonzalezestepa@opendeusto.es>
2019-05-09 11:29:47 +0200

    skere

--- Proyecto76/main.cpp
+++ Proyecto76/main.cpp
@@ -1,12 +1,13 @@
 #include "IaGUI.h"#include "Client2.h"
 #include "MainMenu.h"
 #include "Login.h"
-
+#include "Client2.h"
 int main() {
 
 
 	//IaGUI(true); //true -> IA  |  false -> PvP
 	//MainMenu();
-	Login();
+	//Login();
+	Client2();
 	return 0;
 }
